b42094dcc2e6acf4e8355e0483d0b8de3ebad866
AK <icecicle04@gmail.com>
2020-10-02 22:46:39 -0400

    added nodeman and fs readfiles for notes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post("../notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
   console.log(req.body);
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
 });
 
 app.listen(PORT, () => {
