86cb910106469e4fb3d27448f6d1d55d81b220d7
AK <icecicle04@gmail.com>
2020-10-02 23:28:05 -0400

    added get request

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -8,16 +8,54 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to retrieve notes.",
+      });
+    }
+    res.json({
+      error: false,
+      data: JSON.parse(data),
+      message: "Successfully retrieved notes.",
+    });
+  });
+});
+
 app.post("/api/notes", (req, res) => {
   console.log(req.body);
   fs.readFile("./db.json", "utf-8", (err, data) => {
-    if (err) throw err;
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to add note.",
+      });
+    }
     console.log(data);
     const updatedData = JSON.parse(data);
     updatedData.push(req.body);
     console.log(updatedData);
     fs.writeFile("./db.json", JSON.stringify(updatedData), (err) => {
-      if (err) throw err;
+      if (err)
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            error: true,
+            data: null,
+            message: "Unable to add note.",
+          });
+        }
+      res.json({
+        error: false,
+        data: updatedData,
+        message: "Successfully added new note.",
+      });
     });
   });
 });
