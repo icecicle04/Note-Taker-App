b141910e61bf096c58e4393b95ae690aec170e7a
AK <icecicle04@gmail.com>
2020-10-02 22:10:24 -0400

    started server and fixed package.json file

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/api/students", (req, res) => {
+  console.log(req.body);
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
