efabc001d229071716e59386327486a28a198ec7
AK <icecicle04@gmail.com>
2020-10-04 00:11:00 -0400

    testing link

--- js/server.js
+++ js/server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "notes"));
+  res.sendFile(path.join(__dirname, "sike.html"));
 });
 
 app.get("/api/notes", (req, res) => {
