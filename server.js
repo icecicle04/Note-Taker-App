96c9db2b4bf1c973e338e147a853fd11171b63e8
AK <icecicle04@gmail.com>
2020-10-05 23:25:19 -0400

    reverted changes

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post("/api/notes", (req, res) => {
   fs.readFile("/db.json", "utf-8", (err, data) => {
     if (err) {
       console.log(err);
-      return res.status(250).json({
+      return res.status(500).json({
         error: true,
         data: null,
         message: "Unable to add note.",
@@ -58,7 +58,7 @@ app.post("/api/notes", (req, res) => {
       if (err)
         if (err) {
           console.log(err);
-          return res.status(400).json({
+          return res.status(500).json({
             error: true,
             data: null,
             message: "Unable to add note.",
