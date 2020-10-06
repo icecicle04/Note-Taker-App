374721c43bb52711f3b970c499ae63acefdb88cc
AK <icecicle04@gmail.com>
2020-10-05 23:47:23 -0400

    FIXED THE SAVING OF NOTES

--- server.js
+++ server.js
@@ -53,47 +53,15 @@ app.post("/api/notes", (req, res) => {
   res.json(JSON.parse(updatedData));
 });
 
-// app.post("/api/notes", (req, res) => {
-//   fs.readFile("/db.json", "utf-8", (err, data) => {
-//     if (err) {
-//       console.log(err);
-//       return res.status(500).json({
-//         error: true,
-//         data: null,
-//         message: "Unable to add note.",
-//       });
-//     }
-//     console.log(data);
-//     const updatedData = JSON.parse(data);
-//     updatedData.push(req.body);
-//     // console.log(updatedData);
-//     fs.writeFile("./db.json", JSON.stringify(updatedData), (err) => {
-//       if (err)
-//         if (err) {
-//           console.log(err);
-//           return res.status(500).json({
-//             error: true,
-//             data: null,
-//             message: "Unable to add note.",
-//           });
-//         }
-//       res.json({
-//         error: false,
-//         data: updatedData,
-//         message: "Successfully added new note.",
-//       });
-//     });
-//   });
-// });
 app.delete("api/notes/:id", (req, res) => {
   const deleteNote = req.params.id;
-  updatedData = fs.readFileSync("/db.json", "utf8");
+  updatedData = fs.readFileSync("./db.json", "utf8");
   updatedData = JSON.parse(updatedData);
   updatedData = updatedData.filter(function (note) {
     return note.id != deleteNoteId;
   });
   updatedData = JSON.stringify(updatedData);
-  fs.writeFile("/db.json", updatedData, "utf8", (err) => {
+  fs.writeFile("./db.json", updatedData, "utf8", (err) => {
     if (err) throw err;
   });
   res.json(JSON.parse(updatedData));
